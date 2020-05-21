49173d870be7f1bb8721b436085f28659d143226
Tanil Orhan <torhan15@ku.edu.tr>
2020-05-21 22:19:56 +0300

    Node class dev

---
+++ ModivSimjava/src/Message.java
@@ -0,0 +1,23 @@
+public class Message {
+
+
+    private int receiverID;
+    private int senderID;
+
+
+    public int getReceiverID() {
+        return receiverID;
+    }
+
+    public void setReceiverID(int receiverID) {
+        this.receiverID = receiverID;
+    }
+
+    public int getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(int senderID) {
+        this.senderID = senderID;
+    }
+}
---
+++ ModivSimjava/src/Node.java
@@ -0,0 +1,36 @@
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Node {
+
+    private int nodeID;
+    private Hashtable<Integer,Integer> linkCostTable;
+    private Hashtable<Integer,Integer> linkBandwithTable;
+
+
+    public Node(int nodeID, Hashtable<Integer,Integer> linkCost, Hashtable<Integer,Integer> linkBandwith){
+        this.nodeID=nodeID;
+        this.linkBandwithTable =linkBandwith;
+        this.linkCostTable =linkCost;
+    }
+
+    public void receiveUpdate(Message m){
+
+    }
+
+    public String toString(){
+        String nodeString="nodeID: "+nodeID+"\n";
+        Set<Integer> neighbours=linkCostTable.keySet();
+        Iterator<Integer> itr=neighbours.iterator();
+        while(itr.hasNext()){
+            Integer next=itr.next();
+            nodeString.concat(next.toString());
+            nodeString.concat(" ");
+            nodeString.concat(linkCostTable.get(next).toString());
+            nodeString.concat(" ");
+            nodeString.concat(linkBandwithTable.get(next).toString());
+        }
+        return nodeString+"\n";
+    }
+}
